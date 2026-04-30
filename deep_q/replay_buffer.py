d53f75cbd1275c4c89fd17715b7ad4704d1f8acb
parth <parthmpotdar02@gmail.com>
2026-04-30 16:59:54 +0200

    implemented replay buffer

---
+++ deep_q/replay_buffer.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+class ReplayBuffer:
+    """
+    Experience Replay Buffer to train Q network
+        Store 'experiences' (s_t, a_t, r_t, s_t+1, done) in separate numpy arrays
+        Use fixed-size numpy arrays with pointer -> O(1) store method
+        (using .pop() would be really slow as python has to move all pointers across)
+    """
+    def __init__(self, capacity, state_dim, action_dim):
+        self.capacity = capacity
+
+        self.pointer = 0 # track the next write position for new experience
+        self.size = 0 # current size of memory
+
+        # memory arrays, unpack any tuple inputs using *
+        self.states = np.zeros((self.capacity, *state_dim))
+        self.actions = np.zeros((self.capacity, *action_dim))
+        self.rewards = np.zeros(self.capacity)
+        self.next_states = np.zeros((self.capacity, *state_dim))
+        self.dones = np.zeros(self.capacity)
+    
+    def sample(self, batch_size):
+        """Randomly sample a batch of experiences from the buffer"""
+        # pick random indices to sample for batch
+        indices = np.random.randint(0, self.size, size=batch_size)
+
+        # return batch of experience tuples
+        return (
+            self.states[indices],
+            self.actions[indices],
+            self.rewards[indices],
+            self.next_states[indices],
+            self.dones[indices]
+                            )
+        
+    def store(self, state, action, reward, next_state, done):
+        """
+        Store a new experience in the buffer
+        -> Using pointer, we can just wrap-around using modulo to overwrite oldest experience 
+        """
+
+        # overwrite values at current write pointer
+        self.states[self.pointer] = state
+        self.actions[self.pointer] = action
+        self.rewards[self.pointer] = reward
+        self.next_states[self.pointer] = next_state
+        self.dones[self.pointer] = done
+        
+        # wrap around the pointer if capacity reached
+        self.pointer = (self.pointer + 1) % self.capacity
+
+        # update size of memory
+        self.size = min(self.size + 1, self.capacity)
--- experiments/test.py
+++ experiments/test.py
@@ -1,13 +1,22 @@
-import deep_q
+from deep_q.replay_buffer import ReplayBuffer
 import gymnasium as gym
 import matplotlib.pyplot as plt
 env = gym.make("CartPole-v1", render_mode="human") # render_mode = human -> opens a pygame window
 
-env.reset(seed=123, options={"low": -0.1, "high": 0.1})
+replay_buffer = ReplayBuffer(100, env.observation_space.shape, env.action_space.shape)
 
-print(env.action_space)
-print(env.observation_space)
-terminated = False
-while True:
-    action = env.action_space.sample()  
-    next, reward, terminated, truncated, info = env.step(action)
+for i in range(10):
+    state, info = env.reset(seed=123, options={"low": -0.1, "high": 0.1})
+    terminated = False
+
+    while terminated == False:
+        action = env.action_space.sample()
+        next_state, reward, terminated, truncatated, info = env.step(action)
+
+        replay_buffer.store(state, action, reward, next_state, terminated)
+        state = next_state
+
+print(replay_buffer.size)
+
+batch_size = 15
+sample = replay_buffer.sample(batch_size)
