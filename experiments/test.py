a544bf3d57bbfcb7abc0ba687da47a65c77f3a13
parth <parthmpotdar02@gmail.com>
2026-04-30 11:56:29 +0200

    testing gym cartpole env

--- experiments/test.py
+++ experiments/test.py
@@ -1,1 +1,13 @@
 import deep_q
+import gymnasium as gym
+import matplotlib.pyplot as plt
+env = gym.make("CartPole-v1", render_mode="human") # render_mode = human -> opens a pygame window
+
+env.reset(seed=123, options={"low": -0.1, "high": 0.1})
+
+print(env.action_space)
+print(env.observation_space)
+terminated = False
+while True:
+    action = env.action_space.sample()  
+    next, reward, terminated, truncated, info = env.step(action)
